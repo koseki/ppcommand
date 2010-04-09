ea8c41d98522a342203eaed696f41caf06f5b91a
koseki <koseki@gmail.com>
2010-04-09 21:14:58 +0900

    add --version option. change HTML option to -H.

--- lib/ppcommand.rb
+++ lib/ppcommand.rb
@@ -79,14 +79,25 @@ class PPCommand
     opp = OptionParser.new
 
     opp.banner = "pp [options] [file|URI]"
-    opp.on("-y", "--yaml", "parse YAML and pp."){|x| opts[:type] = "yaml"}
+    opp.on_tail("-h", "--help", "show this help.") do
+      puts opp
+      exit
+    end
+    opp.on_tail("-v", "--version", "show version.") do
+      puts "ppcommand " + File.read(File.dirname(__FILE__) + "/../VERSION")
+      exit
+    end
+
+    opp.on("-c", "--csv", "parse CSV and pp."){|x| opts[:type] = "csv"}
+    opp.on("-C", "--csvtable", "parse CSV, add labels and pp."){|x| opts[:type] = "csvhash"}
+    opp.on("-H", "--html", "parse HTML and pp."){|x| opts[:type] = "html"}
     opp.on("-j", "--json", "parse JSON and pp."){|x| opts[:type] = "json"}
     opp.on("-x", "--xml", "parse XML using REXML and pp."){|x| opts[:type] = "xml"}
     opp.on("-X", "--xmlsimple", "parse XML using XMLSimple and pp."){|x| opts[:type] = "xmlsimple"}
-    opp.on("-c", "--csv", "parse CSV and pp."){|x| opts[:type] = "csv"}
-    opp.on("-C", "--csvtable", "parse CSV, add labels and pp."){|x| opts[:type] = "csvhash"}
-    opp.on("-h", "--html", "parse HTML and pp."){|x| opts[:type] = "html"}
+    opp.on("-y", "--yaml", "parse YAML and pp."){|x| opts[:type] = "yaml"}
+
     opp.on("-t", "--text", "do not parse. print plain text."){|x| opts[:type] = "text"}
+
     opp.parse!(argv)
 
     file = argv.shift
